9de0cf4ec93939bd80234b8b3dc38c2baa5f0e1f
hi <finackieee@users.noreply.github.com>
2016-07-19 12:59:48 -0400

    cya

--- main.py
+++ main.py
@@ -365,7 +365,6 @@ def main():
 
         for x in range(6):
             h = heartbeat(auth_service, api_endpoint, access_token, response)
-            print u'\u2764'
             if x == 5:
                 print("Connection Terminated")
                 return
