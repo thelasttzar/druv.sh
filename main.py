9eeb6c02eda0aacd9f29a07210e49b62ff971154
thelasttzar <tlt353@gmail.com>
2016-07-20 00:46:30 -0500

    Add custom config profiles

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 import scanner
 import time
+import sys
+
 
 def main():
+
+    if len(sys.argv) == 2:
+        account = sys.argv[1]
+    elif len(sys.argv) == 1:
+        account = 'Login1'
+    else:
+        print("[X] Invalid commands sent.")
+        print("[X] Please use either ./main.py or ./main.py profile")
+
     for t in range(20):
         try:
-            scanner.scan()
-        except Exception as e:
-            print("Restarting scanner in 60 seconds.")
-            time.sleep(60)
+            scanner.scan(account)
+        except:
+            pass
+
+        print("Restarting scanner in 60 seconds.")
+        time.sleep(60)
 
 if __name__ == '__main__':
     main()
